150b13fc609d01e37f26cfce013488439648e8a7
Octaviomtc <octavio150@gmail.com>
2014-07-21 20:06:22 -0500

    Primer Commit

---
+++ application/views/includes/css2.php
@@ -0,0 +1,6 @@
+<!-- font-awesome -->
+<link href="<?=base_url()?>/Recursos/css/font-awesome.min.css" rel="stylesheet">
+<link href="<?=base_url()?>/Recursos/css/bootstrap.css" rel="stylesheet">
+<!-- JavaScript -->
+<script type="text/javascript" src="<?=base_url()?>/Recursos/js/jquery-2.1.1.min.js"></script>
+<script type="text/javascript" src="<?=base_url()?>/Recursos/js/bootstrap.js"></script>
