7b5f7d4194d85df668a31af1f10e74b3a2ce01b7
Octaviomtc <octavio150@gmail.com>
2014-08-10 16:05:20 -0500

    API

--- application/controllers/api/users.php
+++ application/controllers/api/users.php
@@ -1,141 +1,309 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-/**
- * Example
- *
- * This is an example of a few basic user interaction methods you could use
- * all done with a hardcoded array.
- *
- * @package		CodeIgniter
- * @subpackage	Rest Server
- * @category	Controller
- * @author		Phil Sturgeon
- * @link		http://philsturgeon.co.uk/code/
-*/
-
-// This can be removed if you use __autoload() in config.php OR use Modular Extensions
 require APPPATH.'/libraries/REST_Controller.php';
 
 class Users extends REST_Controller
 {
-  function login_post(){
 
+  /*Iniciar Sesion*/
+  function login_post(){
+     //Se crea ina sesion Temporal 
     $this->load->model('user','',TRUE);
 
     $email = $this->post('email');
     $pass = $this->post('pass');
 
-
     $result = $this->user->usuario_login($email, $pass);
-
-
-        if($result)
-        {
-            $this->response(array('success' => true, 'usuario' => $result), 200);
-        }
-
-        else
-        {
-            $this->response(array('message' => 'Usuario y/o contraseña incorrectos'), 200);
-        }
+    if($result){$this->response(array('success' => true, 'usuario' => $result), 200);}
+    else{$this->response(array('message' => 'Usuario y/o contraseña incorrectos'), 200);}
     
   }
 
-  function sfl_cv_02_post()
+  /*Cerrar Sesion*/
+  function logout_post(){
+     //Se crea ina sesion Temporal 
+    $this->load->model('user','',TRUE);
+
+    $session = $this->post('session');
+    $id = $this->post('id');
+
+
+    $result = $this->user->usuario_logout($session, $id);
+    if($result){$this->response(array('success' => true), 200);}
+    else{$this->response(array('message' => 'Usuario y/o contraseña incorrectos'), 200);}
+    
+  }
+
+
+
+  /*Ver taabla cat_aspirante*/
+  function cat_aspirante_post()
     {
       //Acceso a modelo para obtener datos
       $this->load->model('user','',TRUE);
       $id = $this->post('id');
-      $result = $this->user->sfl_cv_02($id);
+      $session = $this->post('session');
+
+      /*Se revisa seision*/
+      $sess = $this->user->session($id, $session);
+
+      if ($sess){
+        $result = $this->user->cat_aspirante($id);
         if($result){$this->response(array('success' => true,'usuario' => $result), 200);}
         else{$this->response(array('message' => 'No usuario'), 200);}
+      }else{
+        $this->response(array('message' => 'Inicie Session'), 200);
+      }
     }
 
-  function Usfl_cv_02_post()
+
+    /*Ver taabla cat_aspirante*/
+  function session_post()
+    {
+      //Acceso a modelo para obtener datos
+      $this->load->model('user','',TRUE);
+      $id = $this->post('id');
+      $session = $this->post('session');
+
+      /*Se revisa seision*/
+      $sess = $this->user->session($id, $session);
+
+
+        if($sess){$this->response(array('success' => true), 200);}
+        else{$this->response(array('message' => 'No sesion'), 200);}
+    
+    }
+
+
+
+  /*Actualizar taabla cat_aspirante*/
+  function u_cat_aspirante_post()
     {
       //Acceso a modelo para obtener datos
       $this->load->model('user','',TRUE);
 
+      $data = array(
+            'paterno' => $this->post('paterno'),
+            'materno' => $this->post('materno'),
+            'nombre' => $this->post('nombre'),
+            'fec_nac' => $this->post('fec_nac'),
+            'id_estado' => $this->post('id_estado'),
+            'id_del_mun' => $this->post('id_del_mun'),
+            'zona' => $this->post('zona'),
+            'calle' => $this->post('calle'),
+            'num_ext' => $this->post('num_ext'),
+            'num_int' => $this->post('num_int'),
+            'colonia' => $this->post('colonia'),
+            'cp' => $this->post('cp'),
+            'tel_particular' => $this->post('tel_particular'),
+            'tel_movil' => $this->post('tel_movil'),
+            'tel_oficina' => $this->post('tel_oficina'),
+            'tel_rec' => $this->post('tel_rec'),
+            'twitter' => $this->post('twitter'),
+            'facebook' => $this->post('facebook'),
+            'linkedin' => $this->post('linkedin'),
+            'rfc' => $this->post('rfc'),
+            'curp' => $this->post('curp'),
+            'no_imss' => $this->post('no_imss'),
+            'no_clinica' => $this->post('no_clinica')
+        );
       $id = $this->post('id');
-      $paterno= $this->post('paterno');
-      $materno= $this->post('materno');
-      $nombre= $this->post('nombre');
-      $fec_nac= $this->post('fec_nac');
+      $session= $this->post('session');
       
-      $result = $this->user->Usfl_cv_02($id, $paterno, $materno, $nombre, $fec_nac);
+      /*Se revisa seision*/
+      $sess = $this->user->session($id, $session);
 
+      if ($sess){
+        $result = $this->user->Ucat_aspirante($id, $data);
         if($result){$this->response(array('success' => true,'usuario' => $result), 200);}
         else{$this->response(array('message' => 'No usuario'), 200);}
+      }else{
+        $this->response(array('message' => 'Inicie Session'), 200);
+      }
     }
 
 
-    function sfl_cv_02a_post()
+  /*Ver taabla cat_ref_persona*/
+  function cat_ref_persona_post()
     {
       //Acceso a modelo para obtener datos
       $this->load->model('user','',TRUE);
       $id = $this->post('id');
-      $result = $this->user->sfl_cv_02a($id);
+      $session = $this->post('session');
+
+      /*Se revisa seision*/
+      $sess = $this->user->session($id, $session);
+
+      if ($sess){
+        $result = $this->user->cat_ref_persona($id);
         if($result){$this->response(array('success' => true,'usuario' => $result), 200);}
         else{$this->response(array('message' => 'No usuario'), 200);}
-    }
-
-
-    function Usfl_cv_02a_post()
-    {
-      //Acceso a modelo para obtener datos
-      $this->load->model('user','',TRUE);
-
-      $id = $this->post('id');
-      $id_estado = $this->post('id_estado');
-      $id_del_mun = $this->post('id_del_mun'); 
-      $zona = $this->post('zona'); 
-      $calle = $this->post('calle'); 
-      $num_ext = $this->post('num_ext'); 
-      $num_int = $this->post('num_int'); 
-      $colonia = $this->post('colonia'); 
-      $cp = $this->post('cp');
-
-      $result = $this->user->Usfl_cv_02a($id, $id_estado, $id_del_mun, $zona, $calle, $num_ext, $num_int, $colonia, $cp);
-
-        if($result){$this->response(array('success' => true,'usuario' => $result), 200);}
-        else{$this->response(array('message' => 'No usuario'), 200);}
+      }else{
+        $this->response(array('message' => 'Inicie Session'), 200);
+      }
     }
 
 
 
-    function sfl_cv_03_post()
-    {
-      //Acceso a modelo para obtener datos
-      $this->load->model('user','',TRUE);
-      $id = $this->post('id');
-      $result = $this->user->sfl_cv_03($id);
-        if($result){$this->response(array('success' => true,'usuario' => $result), 200);}
-        else{$this->response(array('message' => 'No usuario'), 200);}
-    }
-
-  function Usfl_cv_03_post()
+     /*Actualizar taabla cat_ref_persona*/
+  function u_cat_ref_persona_post()
     {
       //Acceso a modelo para obtener datos
       $this->load->model('user','',TRUE);
 
+      $data = array(
+            'id_ref' => $this->post('id_ref'),
+            'ref_nom' => $this->post('ref_nom'),
+            'ref_dom' => $this->post('ref_dom'),
+            'ref_tel' => $this->post('ref_tel'),
+            'ref_ocu' => $this->post('ref_ocu'),
+            'time_con' => $this->post('time_con')
+        );
       $id = $this->post('id');
-      $tel_particular= $this->post('tel_particular');
-      $tel_movil= $this->post('tel_movil');
-      $tel_oficina= $this->post('tel_oficina');
-      $tel_rec= $this->post('tel_rec');
-      $twitter= $this->post('twitter');
-      $facebook= $this->post('facebook');
-      $linkedin= $this->post('linkedin');
-      $rfc= $this->post('rfc');
-      $curp= $this->post('curp');
-      $no_imss= $this->post('no_imss');
-      $no_clinica= $this->post('no_clinica');
-
+      $session= $this->post('session');
+      $id_ref= $this->post('id_ref');
       
-      $result = $this->user->Usfl_cv_03($id, $tel_particular, $tel_movil, $tel_oficina, $tel_rec, $twitter, $facebook, $linkedin, $rfc, $curp, $no_imss, $no_clinica);
+      /*Se revisa seision*/
+      $sess = $this->user->session($id, $session);
 
+      if ($sess){
+        $result = $this->user->Ucat_ref_persona($id, $id_ref, $data);
         if($result){$this->response(array('success' => true,'usuario' => $result), 200);}
         else{$this->response(array('message' => 'No usuario'), 200);}
+      }else{
+        $this->response(array('message' => 'Inicie Session'), 200);
+      }
+    }
+
+
+  /*Inserta tabla cat_ref_persona*/
+  function i_cat_ref_persona_post()
+    {
+      //Acceso a modelo para obtener datos
+      $this->load->model('user','',TRUE);
+
+      $data = array(
+            'id_cuenta' => $this->post('id'),
+            'ref_nom' => $this->post('ref_nom'),
+            'ref_dom' => $this->post('ref_dom'),
+            'ref_tel' => $this->post('ref_tel'),
+            'ref_ocu' => $this->post('ref_ocu'),
+            'time_con' => $this->post('time_con')
+        );
+      $id = $this->post('id');
+      $session= $this->post('session');
+      
+      /*Se revisa seision*/
+      $sess = $this->user->session($id, $session);
+
+      if ($sess){
+        $result = $this->user->Icat_ref_persona($data);
+        if($result){$this->response(array('success' => true,'usuario' => $result), 200);}
+        else{$this->response(array('message' => 'No usuario'), 200);}
+      }else{
+        $this->response(array('message' => 'Inicie Session'), 200);
+      }
+    }
+
+
+
+
+    /*Ver taabla cat_ref_persona*/
+  function perf_exp_post()
+    {
+      //Acceso a modelo para obtener datos
+      $this->load->model('user','',TRUE);
+      $id = $this->post('id');
+      $session = $this->post('session');
+
+      /*Se revisa seision*/
+      $sess = $this->user->session($id, $session);
+
+      if ($sess){
+        $result = $this->user->perf_exp($id);
+        if($result){$this->response(array('success' => true,'usuario' => $result), 200);}
+        else{$this->response(array('message' => 'No usuario'), 200);}
+      }else{
+        $this->response(array('message' => 'Inicie Session'), 200);
+      }
+    }
+
+       /*Actualizar taabla cat_ref_persona*/
+  function u_perf_exp_post()
+    {
+      //Acceso a modelo para obtener datos
+      $this->load->model('user','',TRUE);
+
+      $data = array(
+            'lug_nac' => $this->post('lug_nac'),
+            'genero' => $this->post('genero'),
+            'edo_civil' => $this->post('edo_civil'),
+            'auto_prop' => $this->post('auto_prop'),
+            'auto_esta' => $this->post('auto_esta'),
+            'mane_carre' => $this->post('mane_carre'),
+            'dispo_viaj' => $this->post('dispo_viaj'),
+            'dispo_res' => $this->post('dispo_res')
+        );
+      $id = $this->post('id');
+      $session= $this->post('session');
+      
+      /*Se revisa seision*/
+      $sess = $this->user->session($id, $session);
+
+      if ($sess){
+        $result = $this->user->Uperf_exp($id, $data);
+        if($result){$this->response(array('success' => true,'usuario' => $result), 200);}
+        else{$this->response(array('message' => 'No usuario'), 200);}
+      }else{
+        $this->response(array('message' => 'Inicie Session'), 200);
+      }
+    }
+
+
+
+
+
+  /*Ver taabla estados*/
+  function estados_post()
+    {
+      //Acceso a modelo para obtener datos
+      $this->load->model('user','',TRUE);
+
+
+      $result = $this->user->estados();
+        
+        if($result){$this->response(array('success' => true,'estados' => $result), 200);}
+        else{$this->response(array('message' => 'No estados'), 200);}
+   
+    }
+
+    /*Ver taabla estados*/
+  function delegaciones_post()
+    {
+      //Acceso a modelo para obtener datos
+      $this->load->model('user','',TRUE);
+
+      $id_estado=$this->post('id_estado');
+
+      $result = $this->user->delegaciones($id_estado);
+        
+        if($result){$this->response(array('success' => true,'delegaciones' => $result), 200);}
+        else{$this->response(array('message' => 'No delegaciones'), 200);}
+   
+    }
+
+
+      /*Ver taabla zona*/
+  function zona_post()
+    {
+      //Acceso a modelo para obtener datos
+      $this->load->model('user','',TRUE);
+
+
+      $result = $this->user->zona();
+        
+        if($result){$this->response(array('success' => true,'zona' => $result), 200);}
+        else{$this->response(array('message' => 'No zona'), 200);}
+   
     }
 
 
--- application/models/user.php
+++ application/models/user.php
@@ -2,6 +2,7 @@
 Class User extends CI_Model
 {
 
+
  function login($username, $password)
  {
   /*Funcion para validar informacón de inicio de sesión*/
@@ -89,12 +90,101 @@ function vacantes()
 
 
 
-//Obtiene sesion
+
+//Obtiene Todas las vacantes Full
+function vacantes_full()
+{
+ /*Funcion para validar informacón de inicio de sesión*/
+
+  $this -> db -> select('*');
+  $this->db->order_by("fecha","asc");
+  $this -> db -> from('vacantes');
+
+
+  $query = $this -> db -> get();
+
+
+    return $query->result();
+
+}
+
+
+
+
+
+
+
+
+//Obtiene Todas las vacantes
+function reclutadores()
+{
+ /*Funcion para validar informacón de inicio de sesión*/
+
+  $this -> db -> select('*');
+  $this->db->order_by("tipo_user","asc");
+  $this -> db -> from('users');
+
+
+  $query = $this -> db -> get();
+
+
+    return $query->result();
+
+}
+
+
+
+function aspirantes()
+{
+ /*Funcion para validar informacón de inicio de sesión*/
+
+  $this -> db -> select('*');
+  $this->db->order_by("paterno","asc");
+  $this -> db -> from('cat_aspirante');
+
+
+  $query = $this -> db -> get();
+
+
+    return $query->result();
+
+}
+
+
+function insert_reclutador($datos){
+   $data = array(
+                 'username' => $datos['username'],
+                 'password' => $datos['password'],
+                 'tipo_user' => $datos['tipo_user'],
+                 'Nombre' => $datos['Nombre'],
+                 'ApellidoP' => $datos['ApellidoP'],
+                 'ApellidoM' => $datos['ApellidoM'],
+                 'mail' => $datos['mail'],
+                 'telefono' => $datos['telefono'],
+                 'celular' => $datos['celular']
+                   );
+  $this->db->insert('users',$data);
+
+   }
+
+
+/*******************        FUNCIONES PARA API         *************************/
+
+/*Iniciar Session*/
 function usuario_login($user, $pass)
 {
+  $session = '';
+  $pattern = '1234567890abcdefghijklmnopqrstuvwxyz%&#';
+  $max = strlen($pattern)-1;
+  for($i=0;$i < 25;$i++) $session .= $pattern{mt_rand(0,$max)};
+
+   $data = array('session' => $session);
+   $this -> db -> where('email', $user);
+   $this -> db -> where('passw', MD5($pass));
+   $this->db->update('cat_cuenta', $data);
 
  /*Funcion para validar informacón de inicio de sesión*/
-   $this -> db -> select('id_cuenta, email');
+   $this -> db -> select('id_cuenta, email, session');
    $this -> db -> from('cat_cuenta');
    $this -> db -> where('email', $user);
    $this -> db -> where('passw', MD5($pass));
@@ -102,216 +192,147 @@ function usuario_login($user, $pass)
 
    $query = $this -> db -> get();
 
-   if($query -> num_rows() == 1)
-   {
-     return $query->result();
-   }
-   else
-   {
-     return false;
-   }
+   if($query -> num_rows() == 1) {return $query->result();}
+   else{return false;}
 
 }
 
-
-function sfl_cv_02($id){
-  $this -> db -> select('id_aspirante, id_cuenta, paterno, materno, nombre, fec_nac');
-   $this -> db -> from('cat_aspirante');
+/*Cerrar Session*/
+function usuario_logout($session, $id)
+{
+   $data = array('session' => "");
+   $this -> db -> where('session', $session);
    $this -> db -> where('id_cuenta', $id);
-   $this-> db ->order_by('id_aspirante', 'DESC'); 
-  $this-> db ->limit(1);
+   $this->db->update('cat_cuenta', $data);
+   return true;
+}
 
+/*Revisar session*/
+function session($id, $session){
+   $this -> db -> select('*');
+   $this -> db -> from('cat_cuenta');
+   $this -> db -> where('id_cuenta', $id);
+   $this -> db -> where('session', $session);
+
+   $this -> db -> limit(1);
 
    $query = $this -> db -> get();
 
-   if($query -> num_rows() == 1)
-   {
-     return $query->result();
-   }
-   else
-   {
-     return false;
-   }
-
+   if($query -> num_rows() == 1) {return true;}
+   else{return false;}
 }
 
+function cat_aspirante($id){
+  $this -> db -> select('*');
+  $this -> db -> from('cat_aspirante');
+  $this -> db -> where('id_cuenta', $id);
 
-function Usfl_cv_02($id, $paterno, $materno, $nombre, $fec_nac){
-        $data = array(
-            'paterno' => $paterno,
-            'materno' => $materno,
-            'nombre' => $nombre,
-            'fec_nac' => $fec_nac
-        );
-        $this->db->where('id_cuenta', $id);
-        return $this->db->update('cat_aspirante', $data);
-}
-
-
-function sfl_cv_02a($id){
-  $this -> db -> select('id_aspirante, id_cuenta, id_estado, id_del_mun, zona, calle, num_ext, num_int, colonia, cp');
-   $this -> db -> from('cat_aspirante');
-   $this -> db -> where('id_cuenta', $id);
-   $this-> db ->order_by('id_aspirante', 'DESC'); 
+  $this-> db ->order_by('id_aspirante', 'DESC'); 
   $this-> db ->limit(1);
 
 
-   $query = $this -> db -> get();
+  $query = $this -> db -> get();
 
-   if($query -> num_rows() == 1)
-   {
-     return $query->result();
-   }
-   else
-   {
-     return false;
-   }
+  if($query -> num_rows() == 1){return $query->result();}
+  else{return false;}
 
 }
 
 
-
-function Usfl_cv_02a($id, $id_estado, $id_del_mun, $zona, $calle, $num_ext, $num_int, $colonia, $cp){
-        $data = array(
-           'id_estado' =>$id_estado, 
-            'id_del_mun'=>$id_del_mun, 
-           'zona' =>$zona, 
-           'calle' =>$calle, 
-           'num_ext' =>$num_ext, 
-           'num_int' =>$num_int, 
-           'colonia' =>$colonia, 
-           'cp' =>$cp
-        );
-        $this->db->where('id_cuenta', $id);
-        return $this->db->update('cat_aspirante', $data);
+function Ucat_aspirante($id, $data){
+  $this->db->where('id_cuenta', $id);
+  return $this->db->update('cat_aspirante', $data);
 }
 
 
+function cat_ref_persona($id){
+  $this -> db -> select('*');
+  $this -> db -> from('cat_ref_persona');
+  $this -> db -> where('id_cuenta', $id);
+
+  $this-> db ->order_by('id_ref', 'DESC'); 
 
 
-function sfl_cv_03($id){
-  $this -> db -> select('id_aspirante, id_cuenta, tel_particular, tel_movil, tel_oficina, tel_rec, twitter, facebook, linkedin, rfc, curp, no_imss, no_clinica');
 
-   $this -> db -> from('cat_aspirante');
-   $this -> db -> where('id_cuenta', $id);
-   $this-> db ->order_by('id_aspirante', 'DESC'); 
-  $this-> db ->limit(1);
+  $query = $this -> db -> get();
 
-
-   $query = $this -> db -> get();
-
-   if($query -> num_rows() == 1)
-   {
-     return $query->result();
-   }
-   else
-   {
-     return false;
-   }
+  if($query -> num_rows() > 0){return $query->result();}
+  else{return false;}
 
 }
 
 
+//Actualiza  informacion cat_ref_persona
+function Ucat_ref_persona($id, $id_ref, $data){
+  $this->db->where('id_cuenta', $id);
+  $this->db->where('id_ref', $id_ref);
+  return $this->db->update('cat_ref_persona', $data);
+}
 
-function Usfl_cv_03($id, $tel_particular, $tel_movil, $tel_oficina, $tel_rec, $twitter, $facebook, $linkedin, $rfc, $curp, $no_imss, $no_clinica){
-        $data = array(
-           'tel_particular' =>$tel_particular,
-           'tel_movil'=>$tel_movil, 
-           'tel_oficina'=>$tel_oficina, 
-           'tel_rec' =>$tel_rec, 
-           'twitter' =>$twitter, 
-           'facebook' =>$facebook, 
-           'linkedin' =>$linkedin, 
-           'rfc' =>$rfc, 
-           'curp' =>$curp, 
-           'no_imss' =>$no_imss, 
-           'no_clinica' =>$no_clinica
-        );
-        $this->db->where('id_cuenta', $id);
-        return $this->db->update('cat_aspirante', $data);
+//Inserrtar  informacion cat_ref_persona
+function Icat_ref_persona($data){
+  return $this->db->insert('cat_ref_persona',$data);
+}
+
+
+
+function perf_exp($id){
+  $this -> db -> select('*');
+  $this -> db -> from('perf_exp');
+  $this -> db -> where('id_cuenta', $id);
+
+  $this-> db ->order_by('id_per_exp', 'DESC'); 
+  $query = $this -> db -> get();
+
+  if($query -> num_rows() > 0){return $query->result();}
+  else{return false;}
+}
+
+//Actualiza  informacion perf_exp
+function Uperf_exp($id, $data){
+  $this->db->where('id_cuenta', $id);
+  return $this->db->update('perf_exp', $data);
 }
 
 
 
 
-// function sfl_cv_02a($id){
-//   $this -> db -> select('id_aspirante, id_cuenta, id_estado, id_del_mun, zona, calle, num_ext, num_int, colonia, cp, tel_particular, tel_movil, tel_oficina, tel_rec, twitter, facebook, linkedin, rfc, curp, no_imss, no_clinica');
-//    $this -> db -> from('cat_aspirante');
-//    $this -> db -> where('id_cuenta', $id);
-//    $this-> db ->order_by('id_aspirante', 'DESC'); 
-//   $this-> db ->limit(1);
+function estados(){
+  $this -> db -> select('*');
+  $this -> db -> from('cat_estados');
+
+  $this-> db ->order_by('estado', 'ASC'); 
+  $query = $this -> db -> get();
+
+  if($query -> num_rows() > 0){return $query->result();}
+  else{return false;}
+}
 
 
-//    $query = $this -> db -> get();
-
-//    if($query -> num_rows() == 1)
-//    {
-//      return $query->result();
-//    }
-//    else
-//    {
-//      return false;
-//    }
-
-// }
+function delegaciones($id_estado){
+  $this -> db -> select('*');
+  $this -> db -> from('cat_mun_del');
+  $this -> db -> where('id_estado', $id_estado);
 
 
-// //Obtiene datos de usuario
-// function usuarioInfo($id)
-// {
+  $this-> db ->order_by('del_mun', 'ASC'); 
+  $query = $this -> db -> get();
 
-//   Tablas de informacion
-//   cat_cuenta select('id_cuenta, email, passw');
-//   cat_aspirante '$id_cuenta',
-//                 '$paterno',
-//                 '$materno',
-//                 '$nombre',
-//                 '$fec_nac',
-//                 
-//                 
-//                 
-//                 
-//                 id_estado ='" . $id_estado . "',
-//                 id_del_mun ='" . $id_del_mun . "',
-//                 zona='" . $id_cat_zonas ."',
-//                 calle ='" . $calle . "',
-//                 num_ext ='" . $num_ext . "',
-//                 num_int ='" . $num_int . "',
-//                 colonia ='" . $colonia. "',
-//                 cp ='" . $cp
-//                 tel_particular ='" . $tel_particular . "',
-//                 tel_movil ='" . $tel_movil . "',
-//                 tel_oficina ='" . $tel_oficina . "',
-//                   tel_rec ='" . $tel_reca . "',
-//                     twitter ='" . $twitter . "',
-//                 facebook ='" . $facebook . "',
-//                   linkedin ='" . $linkedin . "',
-//                 rfc ='" . $rfc . "',
-//                 curp ='" . $curp . "',
-//                 no_imss ='" . $no_imss . "',
-//                 no_clinica ='" . $no_clinica . "'
-//                 
-//                 
-//                 
-//   cat_ref_persona 
-//                 id_ref,
-//               id_cuenta,
-//                 ref_nom,
-//                 ref_dom,
-//                 ref_tel,
-//                 ref_ocu,
-//                 time_con
-//   perf_exp
-//             id_per_exp,
-//                 id_cuenta,
-//                 lug_nac,
-//                 genero,
-//                 edo_civil,
-//                 auto_prop,
-//                 auto_esta,
-//                 mane_carre,
-//                 dispo_viaj,
-//                 dispo_res
+  if($query -> num_rows() > 0){return $query->result();}
+  else{return false;}
+}
+
+function zona(){
+  $this -> db -> select('*');
+  $this -> db -> from('cat_zonas');
+
+  $this-> db ->order_by('id_cat_zonas', 'ASC'); 
+  $query = $this -> db -> get();
+
+  if($query -> num_rows() > 0){return $query->result();}
+  else{return false;}
+}
+
 //   app_industria
 //             id_industria,
 //                 id_cuenta,
@@ -430,45 +451,6 @@ function Usfl_cv_03($id, $tel_particular, $tel_movil, $tel_oficina, $tel_rec, $t
 
 
 
-//  /*Funcion para validar informacón de inicio de sesión*/
-//    $this -> db -> select('id_cuenta, email');
-//    $this -> db -> from('cat_cuenta');
-//    $this -> db -> where('email', $user);
-//    $this -> db -> where('passw', MD5($pass));
-//    $this -> db -> limit(1);
-
-//    $query = $this -> db -> get();
-
-//    if($query -> num_rows() == 1)
-//    {
-//      return $query->result();
-//    }
-//    else
-//    {
-//      return false;
-//    }
-
-// }
-
-
-
-
-//Obtiene Todas las vacantes Full
-function vacantes_full()
-{
- /*Funcion para validar informacón de inicio de sesión*/
-
-  $this -> db -> select('*');
-  $this->db->order_by("fecha","asc");
-  $this -> db -> from('vacantes');
-
-
-  $query = $this -> db -> get();
-
-
-    return $query->result();
-
-}
 
 
 
@@ -477,57 +459,8 @@ function vacantes_full()
 
 
 
-//Obtiene Todas las vacantes
-function reclutadores()
-{
- /*Funcion para validar informacón de inicio de sesión*/
-
-  $this -> db -> select('*');
-  $this->db->order_by("tipo_user","asc");
-  $this -> db -> from('users');
 
 
-  $query = $this -> db -> get();
-
-
-    return $query->result();
-
-}
-
-
-
-function aspirantes()
-{
- /*Funcion para validar informacón de inicio de sesión*/
-
-  $this -> db -> select('*');
-  $this->db->order_by("paterno","asc");
-  $this -> db -> from('cat_aspirante');
-
-
-  $query = $this -> db -> get();
-
-
-    return $query->result();
-
-}
-
-
-function insert_reclutador($datos){
-   $data = array(
-                 'username' => $datos['username'],
-                 'password' => $datos['password'],
-                 'tipo_user' => $datos['tipo_user'],
-                 'Nombre' => $datos['Nombre'],
-                 'ApellidoP' => $datos['ApellidoP'],
-                 'ApellidoM' => $datos['ApellidoM'],
-                 'mail' => $datos['mail'],
-                 'telefono' => $datos['telefono'],
-                 'celular' => $datos['celular']
-                   );
-  $this->db->insert('users',$data);
-
-   }
 
 
 }
